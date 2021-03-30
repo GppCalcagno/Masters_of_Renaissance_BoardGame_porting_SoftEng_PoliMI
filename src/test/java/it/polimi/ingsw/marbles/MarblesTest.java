94c3be3521cf2de12d93aaec974313d319e876c5
GiovanniChiurco <10659071@polimi.it>
2021-03-30 10:09:22 +0200

    insert test package

---
+++ src/test/java/it/polimi/ingsw/marbles/MarblesTest.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.marbles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarblesTest {
+
+    @Test
+    void addtoWarehouse() {
+    }
+
+    @Test
+    void discard() {
+    }
+}
---
+++ src/test/java/stub/stubPlayer.java
@@ -0,0 +1,4 @@
+package stub;
+
+public class stubPlayer {
+}
