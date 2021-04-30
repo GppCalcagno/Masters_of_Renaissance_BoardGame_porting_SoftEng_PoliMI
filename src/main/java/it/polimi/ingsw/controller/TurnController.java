95e2adeb324f13c912bb622fc277a651076b6bea
Giuseppe-Calcagno <79711772+Giuseppe-Calcagno@users.noreply.github.com>
2021-04-30 18:26:31 +0200

    Turn Controller Fix

--- src/main/java/it/polimi/ingsw/controller/TurnController.java
+++ src/main/java/it/polimi/ingsw/controller/TurnController.java
@@ -1,13 +1,10 @@
 package it.polimi.ingsw.controller;
-
-
 import it.polimi.ingsw.model.card.DevelopmentCard;
 import it.polimi.ingsw.model.card.LeaderAction;
 import it.polimi.ingsw.model.exceptions.ActiveVaticanReportException;
 import it.polimi.ingsw.model.exceptions.GameFinishedException;
 import it.polimi.ingsw.model.exceptions.NegativeQuantityExceptions;
 import it.polimi.ingsw.model.exceptions.OverflowQuantityExcepions;
-import it.polimi.ingsw.model.game.DevCardsDeck;
 import it.polimi.ingsw.model.game.Game;
 import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.producible.Coins;
@@ -21,16 +18,18 @@ import java.util.List;
 import java.util.Map;
 
 public class TurnController {
-
-    private DevelopmentCard c;
-
     private Game game;
     private boolean[] AlreadyActivedDevCard;
+    private DevelopmentCard CurrentDevCardPurchase;
     private int currentDevCardProduction;
     private int numPlayersCount;
 
+    /**
+     * this is the controller of TurnController
+     * @throws IOException
+     */
     public TurnController() throws IOException {
-        //game = new Game();
+        game = new Game();
         AlreadyActivedDevCard= new boolean[3];
         numPlayersCount = 0;
     }
@@ -43,6 +42,7 @@ public class TurnController {
     public boolean VerifyNumPlayers (int numPlayers) throws IOException {
         if (numPlayers < 1 || numPlayers > 4)
             return false;
+
         else if (numPlayers == 1) {
             numPlayersCount = numPlayers;
             game = new SinglePlayerGame();
@@ -55,42 +55,48 @@ public class TurnController {
         }
     }
 
+
     /**
      * this method called by the player allow to select from the DevCardDeck the card the player wants to buy
-     * @param x the row
-     * @param y the column
+     * @param row the row
+     * @param column the column
      * @return true if there are no errors
      */
-    public boolean selectDevCard(int x, int y){
+    public boolean selectDevCard(int row, int column){
         try{
-            c=game.getDevelopmentCardDeck().getDevCards(x,y);
-        } catch (NullPointerException e){
-            e.getMessage();
-        } catch (IndexOutOfBoundsException eo){
-            eo.getMessage();
-        }
-        if(game.getCurrentPlayer().getSlotDevCards().maxLevelPurchase(c) && c.getCost().checkResources(game.getCurrentPlayer())) return true;
-        else return false;
+            CurrentDevCardPurchase =game.getDevelopmentCardDeck().getDevCards(row,column);
+        } catch (NullPointerException e){ return false;}
+
+        //controllo che il giocare abbia spazione nello SlotDevCard e che abbia le risorse necessarie
+        return (game.getCurrentPlayer().getSlotDevCards().maxLevelPurchase(CurrentDevCardPurchase) && CurrentDevCardPurchase.getCost().checkResources(game.getCurrentPlayer()));
     }
 
     /**
      * this method called by the player allow to insert the card just bought into it's SlotDevCards
-     * @param y column for SlotDevCards
+     * @param col column for SlotDevCards
      * @return true if there are no error
      */
-    public boolean insertCard(int y) {
-        if (y >= 0 && y <= 2) {
-            if (game.getCurrentPlayer().getSlotDevCards().insertCards(y, c)) return true;
-            else return false;
+    public boolean insertCard(int col) {
+        if (col >= 0 && col <= 2) {
+            return game.getCurrentPlayer().getSlotDevCards().insertCards(col, CurrentDevCardPurchase);
         }
-        else return false;
+        return false;
     }
 
-    /*public boolean deleteResourcesCost(Map<String,Integer> WarehouseRes, Map<String,Integer> StrongboxRes, Map<String,Integer> ExtrachestMap){
-        //controlla se giocatore ha risorse
-        if(!(game.getCurrentPlayer().checkListResources(WarehouseRes,StrongboxRes,ExtrachestMap))) return false;
-    }*/
+    /**
+     * this method is used to pay DevCard Purchase
+     * @param WarehouseRes resources from WarehouseRes
+     * @param StrongboxRes resources from StrongboxRes
+     * @param ExtrachestMap resources from ExtrachestMap
+     * @return true if the resources are correct for  DevCard Purchase
+     */
+    public boolean chooseResourcesForPurchase(Map<String,Integer> WarehouseRes, Map<String,Integer> StrongboxRes, Map<String,Integer> ExtrachestMap){
+        //controllo se le risorse siano corrette
+        if(!CurrentDevCardPurchase.getCost().checkResources(WarehouseRes,StrongboxRes,ExtrachestMap)) return false;
 
+        //elimino risorse dal giocatore
+        return deleteRes(WarehouseRes,StrongboxRes,ExtrachestMap);
+    }
 
     /**
      * this method called by the player allow to activate a leaderActionCard
@@ -98,10 +104,20 @@ public class TurnController {
      * @return true if the card is activated
      */
     public boolean activeLeaderAction(int pos){
-        game.getCurrentPlayer().getLeaderActionBox().get(pos).setActivated();
+        //controllo se posizione valida
+        if(pos>1)return false;
+        LeaderAction card= game.getCurrentPlayer().getLeaderActionBox().get(pos);
 
-        if(game.getCurrentPlayer().getLeaderActionBox().get(pos).getActivated()) return true;
-        else return false;
+        //carta già attivata
+        if(card.getActivated())return true;
+
+        //controllo risorse
+        if(!card.getCost().checkResources(game.getCurrentPlayer()))return false;
+        //attivo carta
+        card.setActivated();
+
+        //potrei fare return true ma lo uso per un "ulteriore controllo"
+        return card.getActivated();
     }
 
     /**
@@ -109,84 +125,112 @@ public class TurnController {
      * @param pos which leader action card must to be discarded: 0 for the first, 1 for the second
      * @return true if the card is discarded
      */
-    public boolean discardLeaderAction(int pos){
-        int s = game.getCurrentPlayer().getLeaderActionBox().size();
-        if(s==0) return false;
-        else {
+    public boolean discardLeaderAction(int pos) throws GameFinishedException {
+        int size = game.getCurrentPlayer().getLeaderActionBox().size();
+        //verifico se indice corretto
+        if(pos<size) return false;
+
             game.getCurrentPlayer().discardLeaderAction(pos);
-            game.getCurrentPlayer().addVictoryPoints(1);
+
+        try {
+            game.getCurrentPlayer().increasefaithMarker();
+        } catch (ActiveVaticanReportException e) {
+            //vedere il singleplayer
+            game.getFaithTrack().checkPopeSpace(game.getPlayersList(),0);
         }
-        if(game.getCurrentPlayer().getLeaderActionBox().size()== s-1) return true;
-        else return false;
+        return true;
     }
 
-    public boolean activeProductionDevCard(int i){
-        if (AlreadyActivedDevCard[i])return false;
 
-        DevelopmentCard card= game.getCurrentPlayer().getSlotDevCards().getDevCards(i);
+    /**
+     * this method is called by client to active a DevCardProduction
+     * @param col is the top item of the selected column of SlotDevCard
+     * @return true if the player could active DevCardProduction
+     */
+    public boolean activeProductionDevCard(int col){
+        if (AlreadyActivedDevCard[col])return false;
 
-        if(card.getCostProduction().checkResources(game.getCurrentPlayer()) && game.getCurrentPlayer().getSlotDevCards().checkUsage(card)){
-            currentDevCardProduction=i;
+        DevelopmentCard card= game.getCurrentPlayer().getSlotDevCards().getDevCards(col);
+
+        if( card.getCostProduction().checkResources(game.getCurrentPlayer()) && game.getCurrentPlayer().getSlotDevCards().checkUsage(card)){
+            currentDevCardProduction=col;
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
+
     }
 
+
+    /**
+     * this method is used to pay DevCardProduction: remove resources from the player and fill SlotDevCard_buffer
+     * @param WarehouseRes resources from WarehouseRes
+     * @param StrongboxRes resources from StrongboxRes
+     * @param ExtrachestMap resources from ExtrachestMap
+     * @return true if the resources are correct for  DevCardProduction
+     */
     public boolean chooseResourcesForProduction(Map<String,Integer> WarehouseRes, Map<String,Integer> StrongboxRes, Map<String,Integer> ExtrachestMap){
         DevelopmentCard card= game.getCurrentPlayer().getSlotDevCards().getDevCards(currentDevCardProduction);
+
+        //controlla se risorse corrispondono a costo carta
+        if(card.getCostProduction().checkResources(WarehouseRes,StrongboxRes,ExtrachestMap))return false;
+
+        //elimino le risorse
+        if(!deleteRes(WarehouseRes,StrongboxRes,ExtrachestMap)) return false;
+
+        //assegno le risorse al buffer giocatore
+        game.getCurrentPlayer().getSlotDevCards().cardProduction(card);
+
+        AlreadyActivedDevCard[currentDevCardProduction]=true;
+        return true;
+    }
+
+    /**
+     * remove Resources from Player
+     * @param WarehouseRes  warehouse selected resources
+     * @param StrongboxRes strongbox selected resources
+     * @param ExtrachestMap extrachest selected resources
+     * @return true if all resources are deleted
+     */
+    public boolean deleteRes(Map<String,Integer> WarehouseRes, Map<String,Integer> StrongboxRes, Map<String,Integer> ExtrachestMap){
 
         //controlla se giocatore ha risorse
         if(!(game.getCurrentPlayer().checkListResources(WarehouseRes,StrongboxRes,ExtrachestMap))) return false;
 
-        //controlla se risorse corrispondono a costo carta
-        if(card.getCostProduction().checkResources(WarehouseRes,StrongboxRes,ExtrachestMap)){
+        //warehouse
+        for(String res: WarehouseRes.keySet()){
+            int index=0;
+            //qui ho hardcodato le risorse
+            Resources[]  ResList={new Coins(),new Shields(), new Servants(), new Servants()};
+            while(!ResList[index].toString().equals(res))index++;
 
-            //assegno le risorse al buffer giocatore
-            game.getCurrentPlayer().getSlotDevCards().cardProduction(card);
-
-            //tolgo le risorse al giocatore:
-
-            //warehouse
-            for(String res: WarehouseRes.keySet()){
-                int index=0;
-                //qui ho hardcodato le risorse
-                Resources[]  ResList={new Coins(),new Shields(), new Servants(), new Servants()};
-                while(!ResList[index].toString().equals(res))index++;
-
-                for(int j=0;j<WarehouseRes.get(res);j++)
-                    game.getCurrentPlayer().getWarehouse().delete(ResList[index]);
-            }
-
-            //strongbox
-            for(String res: StrongboxRes.keySet()){
-                int old=game.getCurrentPlayer().getStrongbox().getNumResources(res);
-                try {
-                    game.getCurrentPlayer().getStrongbox().updateResources(res,old-(StrongboxRes.get(res)));
-                } catch (NegativeQuantityExceptions ignored) {}//ho controllato prima che il giocatore abbia le risorse
-            }
-
-            //extrachest
-            for(String res: ExtrachestMap.keySet()){
-                int Resindex=0;
-                //qui ho hardcodato le risorse
-                Resources[]  ResList={new Coins(),new Shields(), new Servants(), new Servants()};
-                while(!ResList[Resindex].toString().equals(res))Resindex++;
-
-                int ChestIndex=0;
-                while(game.getCurrentPlayer().getWarehouse().getLeaderCardEffect().get(ChestIndex).getResources().toString().equals(res))ChestIndex++;
-                //so che l'indice c'è perchè faccio controllo prima
-
-                int old= game.getCurrentPlayer().getWarehouse().getLeaderCardEffect().get(ChestIndex).getnum();
-                try {
-                    game.getCurrentPlayer().getWarehouse().getLeaderCardEffect().get(ChestIndex).updateResources(old -ExtrachestMap.get(res));
-                } catch (OverflowQuantityExcepions | NegativeQuantityExceptions ignored) {}//non uso i catch perchè controllo prima le risorse
-            }
-            AlreadyActivedDevCard[currentDevCardProduction]=true;
-            return true;
+            for(int j=0;j<WarehouseRes.get(res);j++)
+                game.getCurrentPlayer().getWarehouse().delete(ResList[index]);
         }
-        return false;
+
+        //strongbox
+        for(String res: StrongboxRes.keySet()){
+            try {
+                game.getCurrentPlayer().getStrongbox().updateResources(res,-StrongboxRes.get(res));
+            } catch (NegativeQuantityExceptions ignored) {}//ho controllato prima che il giocatore abbia le risorse
+        }
+
+        //extrachest
+        for(String res: ExtrachestMap.keySet()){
+            int Resindex=0;
+            //qui ho hardcodato le risorse
+            Resources[]  ResList={new Coins(),new Shields(), new Servants(), new Servants()};
+            while(!ResList[Resindex].toString().equals(res))Resindex++;
+
+            int ChestIndex=0;
+            while(game.getCurrentPlayer().getWarehouse().getLeaderCardEffect().get(ChestIndex).getResources().toString().equals(res))ChestIndex++;
+            //so che l'indice c'è perchè faccio controllo prima
+
+            int old= game.getCurrentPlayer().getWarehouse().getLeaderCardEffect().get(ChestIndex).getnum();
+            try {
+                game.getCurrentPlayer().getWarehouse().getLeaderCardEffect().get(ChestIndex).updateResources(old -ExtrachestMap.get(res));
+            } catch (OverflowQuantityExcepions | NegativeQuantityExceptions ignored) {}//non uso i catch perchè controllo prima le risorse
+        }
+        return true;
     }
 
     /**
@@ -206,7 +250,7 @@ public class TurnController {
 
     /**
      * This method permits to the client to add or to discard a caught marbles
-     * @param choice         1 if the client want to add the marble, 0 if he want to discard the marble
+     * @param choice  1 if the client want to add the marble, 0 if he want to discard the marble
      * @param indexwarehouse row's index in which the client want to add the marble
      * @return true if the add is right
      */
@@ -284,10 +328,33 @@ public class TurnController {
         return false;
     }
 
+    /**
+     * this method is used to put buffer resources in Player Strongbox
+     */
+    public void emptyBuffer(){
+        Map<String,Integer> buffer= game.getCurrentPlayer().getSlotDevCards().getBuffer();
+
+        for (String res: buffer.keySet()){
+            try {
+                game.getCurrentPlayer().getStrongbox().updateResources(res,buffer.get(res));
+            } catch (NegativeQuantityExceptions ignored) {} //so che sono tutte positive
+        }
+        buffer.clear();
+    }
+
+
+    /**
+     * getter of game
+     * @return game
+     */
     public Game getGame() {
         return game;
     }
 
+    /**
+     * getter of numPlayersCount
+     * @return numPlayersCount
+     */
     public int getNumPlayersCount() {
         return numPlayersCount;
     }
