4c5fb8ef0d96b9eb4da21d7a1b61208273d04606
Giuseppe-Calcagno <79711772+Giuseppe-Calcagno@users.noreply.github.com>
2021-05-05 12:04:08 +0200

    Added Message

--- src/main/java/it/polimi/ingsw/Network/message/MessagePing.java
+++ src/main/java/it/polimi/ingsw/Network/message/MessagePing.java
@@ -4,6 +4,6 @@ public class MessagePing extends Message{
     private static final long serialVersionUID= -8052982982220258094L;
 
     public MessagePing() {
-        super(null,MessageType.PING);
+        super(null,MessageType.PING) ;
     }
 }
