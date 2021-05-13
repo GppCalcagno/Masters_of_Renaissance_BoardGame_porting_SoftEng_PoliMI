a73f1903c926a30201d36c2c31aa7b1559dc8c4b
GiovanniChiurco <giovanni.chiurco@mail.polimi.it>
2021-05-13 22:40:38 +0200

    added cards to PlayerBoard with search methods and tests

--- src/main/java/it/polimi/ingsw/Client/ClientController.java
+++ src/main/java/it/polimi/ingsw/Client/ClientController.java
@@ -35,8 +35,6 @@ public class ClientController implements Observer {
     ClientSocket clientSocket;
     view view;
     boolean activatedProduction;
-    Map<String, DevelopmentCard> developmentCardMap;
-    Map<String, LeaderAction> leaderActionMap;
 
     MessageType currState;
 
@@ -46,10 +44,6 @@ public class ClientController implements Observer {
         this.board= new PlayerBoard();
         activatedProduction=false;
 
-        developmentCardMap = new HashMap<>();
-        initializeDevCardMap();
-        leaderActionMap = new HashMap<>();
-        initializeLeaderCardMap();
         action();
         currState=MessageType.CONNECT;
     }
@@ -490,126 +484,5 @@ public class ClientController implements Observer {
         Map<String, Integer> extraChest = message.getExtraChest();
         Map<String,Integer> strongbox = message.getStrongbox();
         board.updateresoruces(warehouse,extraChest,strongbox);
-    }
-
-    void initializeDevCardMap() throws IOException {
-        Gson gson = new GsonBuilder().create();
-
-        String devCards3g = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level3green.json"));
-        DevelopmentCard[] devCards3gvet = gson.fromJson(devCards3g, DevelopmentCard[].class);
-        for (DevelopmentCard dc3g : devCards3gvet) {
-            developmentCardMap.put(dc3g.getID(), dc3g);
-        }
-        String devCards3b = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level3blue.json"));
-        DevelopmentCard[] devCards3bvet = gson.fromJson(devCards3b, DevelopmentCard[].class);
-        for (DevelopmentCard dc3b : devCards3bvet) {
-            developmentCardMap.put(dc3b.getID(), dc3b);
-        }
-        String devCards3y = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level3yellow.json"));
-        DevelopmentCard[] devCards3yvet = gson.fromJson(devCards3y, DevelopmentCard[].class);
-        for (DevelopmentCard dc3y : devCards3yvet) {
-            developmentCardMap.put(dc3y.getID(), dc3y);
-        }
-        String devCards3p = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level3purple.json"));
-        DevelopmentCard[] devCards3pvet = gson.fromJson(devCards3p, DevelopmentCard[].class);
-        for (DevelopmentCard dc3p : devCards3pvet) {
-            developmentCardMap.put(dc3p.getID(), dc3p);
-        }
-        String devCards2g = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level2green.json"));
-        DevelopmentCard[] devCards2gvet = gson.fromJson(devCards2g, DevelopmentCard[].class);
-        for (DevelopmentCard dc2g : devCards2gvet) {
-            developmentCardMap.put(dc2g.getID(), dc2g);
-        }
-        String devCards2b = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level2blue.json"));
-        DevelopmentCard[] devCards2bvet = gson.fromJson(devCards2b, DevelopmentCard[].class);
-        for (DevelopmentCard dc2b : devCards2bvet) {
-            developmentCardMap.put(dc2b.getID(), dc2b);
-        }
-        String devCards2y = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level2yellow.json"));
-        DevelopmentCard[] devCards2yvet = gson.fromJson(devCards2y, DevelopmentCard[].class);
-        for (DevelopmentCard dc2y : devCards2yvet) {
-            developmentCardMap.put(dc2y.getID(), dc2y);
-        }
-        String devCards2p = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level2purple.json"));
-        DevelopmentCard[] devCards2pvet = gson.fromJson(devCards2p, DevelopmentCard[].class);
-        for (DevelopmentCard dc2p : devCards2pvet) {
-            developmentCardMap.put(dc2p.getID(), dc2p);
-        }
-        String devCards1g = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level1green.json"));
-        DevelopmentCard[] devCards1gvet = gson.fromJson(devCards1g, DevelopmentCard[].class);
-        for (DevelopmentCard dc1g : devCards1gvet) {
-            developmentCardMap.put(dc1g.getID(), dc1g);
-        }
-        String devCards1b = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level1blue.json"));
-        DevelopmentCard[] devCards1bvet = gson.fromJson(devCards1b, DevelopmentCard[].class);
-        for (DevelopmentCard dc1b : devCards1bvet) {
-            developmentCardMap.put(dc1b.getID(), dc1b);
-        }
-        String devCards1y = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level1yellow.json"));
-        DevelopmentCard[] devCards1yvet = gson.fromJson(devCards1y, DevelopmentCard[].class);
-        for (DevelopmentCard dc1y : devCards1yvet) {
-            developmentCardMap.put(dc1y.getID(), dc1y);
-        }
-        String devCards1p = Files.readString(Paths.get("src/cardJSON/developmentCard/devCards_level1purple.json"));
-        DevelopmentCard[] devCards1pvet = gson.fromJson(devCards1p, DevelopmentCard[].class);
-        for (DevelopmentCard dc1p : devCards1pvet) {
-            developmentCardMap.put(dc1p.getID(), dc1p);
-        }
-    }
-
-    void initializeLeaderCardMap () throws IOException {
-        RuntimeTypeAdapterFactory<Requirements> requirementsRuntimeTypeAdapterFactory = RuntimeTypeAdapterFactory.of(Requirements.class)
-                .registerSubtype(RequestedResources.class)
-                .registerSubtype(RequestedLevelDevelopmentCards.class)
-                .registerSubtype(RequestedTypeDevelopmentCards.class);
-        RuntimeTypeAdapterFactory<Resources> resourcesRuntimeTypeAdapterFactory = RuntimeTypeAdapterFactory.of(Resources.class)
-                .registerSubtype(Coins.class)
-                .registerSubtype(Servants.class)
-                .registerSubtype(Shields.class)
-                .registerSubtype(Stones.class);
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapterFactory(requirementsRuntimeTypeAdapterFactory)
-                .registerTypeAdapterFactory(resourcesRuntimeTypeAdapterFactory)
-                .create();
-
-        String discount = Files.readString(Paths.get("src/cardJSON/leaderCard/VettDiscountLeader_LeaderCards.json"));
-        DiscountLeader[] discountLeaders = gson.fromJson(discount, DiscountLeader[].class);
-        for (LeaderAction dl : discountLeaders) {
-            leaderActionMap.put(dl.getID(), dl);
-        }
-
-        String chest = Files.readString(Paths.get("src/cardJSON/leaderCard/VettChestLeader_LeaderCards.json"));
-        ChestLeader[] chestLeaders = gson.fromJson(chest, ChestLeader[].class);
-        for (LeaderAction cl : chestLeaders) {
-            leaderActionMap.put(cl.getID(), cl);
-        }
-
-        String production = Files.readString(Paths.get("src/cardJSON/leaderCard/VettProductionLeader_LeaderCards.json"));
-        ProductionLeader[] productionLeaders = gson.fromJson(production, ProductionLeader[].class);
-        for (LeaderAction pl : productionLeaders) {
-            leaderActionMap.put(pl.getID(), pl);
-        }
-
-        String transformation = Files.readString(Paths.get("src/cardJSON/leaderCard/VettTrasformationMarbleLeader_LeaderCards.json"));
-        TrasformationMarbleLeader[] transformationMarbleLeaders = gson.fromJson(transformation, TrasformationMarbleLeader[].class);
-        for (LeaderAction tl : transformationMarbleLeaders) {
-            leaderActionMap.put(tl.getID(), tl);
-        }
-    }
-
-    DevelopmentCard searchDevCard (String ID) {
-        return developmentCardMap.get(ID);
-    }
-
-    LeaderAction searchLeaderCard (String ID) {
-        return leaderActionMap.get(ID);
-    }
-
-    public Map<String, DevelopmentCard> getDevelopmentCardMap() {
-        return developmentCardMap;
-    }
-
-    public Map<String, LeaderAction> getLeaderActionMap() {
-        return leaderActionMap;
     }
 }
--- src/test/java/it/polimi/ingsw/Client/PlayerBoardTest.java
+++ src/test/java/it/polimi/ingsw/Client/PlayerBoardTest.java
@@ -9,25 +9,20 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClientControllerTest {
-    PlayerBoard playerBoard = new PlayerBoard();
-    view cli = new Cli(playerBoard);
-
-    ClientControllerTest() throws IOException {
-    }
+class PlayerBoardTest {
 
     @Test
     void searchDevCard() throws IOException {
-        ClientController controller = new ClientController(cli);
-        DevelopmentCard d = controller.searchDevCard("DCL1B1");
+        PlayerBoard playerBoard = new PlayerBoard();
+        DevelopmentCard d = playerBoard.searchDevCard("DCL1B1");
 
         assertEquals(2, d.getCost().getReqMap().get("Coins"));
     }
 
     @Test
     void searchLeaderCard() throws IOException {
-        ClientController controller = new ClientController(cli);
-        LeaderAction l = controller.searchLeaderCard("LCPL1");
+        PlayerBoard playerBoard = new PlayerBoard();
+        LeaderAction l = playerBoard.searchLeaderCard("LCPL1");
 
         assertEquals("LCPL1", l.getID());
     }
