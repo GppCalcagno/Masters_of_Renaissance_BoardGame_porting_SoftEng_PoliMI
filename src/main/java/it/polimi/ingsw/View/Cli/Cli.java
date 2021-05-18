ec905cc1e65ebcb65d55346b07b133a7eca8de94
NicoloCaironi <1021084@polimi.it>
2021-05-18 12:17:07 +0200

    update cli

--- src/main/java/it/polimi/ingsw/View/Cli/Cli.java
+++ src/main/java/it/polimi/ingsw/View/Cli/Cli.java
@@ -26,11 +26,11 @@ public class Cli implements ViewInterface {
     @Override
     public void askServerInfo() {
         out.println("\n");
-        out.println("Enter server IP [default: 127.0.0.1]: ");
+        out.println("Please enter server IP [default: 127.0.0.1]: ");
         String serverAddress = input.nextLine();
 
         out.println("\n");
-        out.println("Enter server port [default : 1234]: ");
+        out.println("Please enter server port [default : 1234]: ");
         int serverPort = input.nextInt();
 
         controller.sendMessage(new MessageConnect(serverAddress, serverPort));
@@ -38,518 +38,40 @@ public class Cli implements ViewInterface {
 
     @Override
     public void endturn() {
+        controller.sendMessage(new MessageEndTurn(playerBoard.getNickname()));
 
     }
 
-    @Override
+    @Override  //nickname + num player + gamestart + first resources and leader action tutto in un unico metodo non è meglio??
     public void askNickname() {
         out.println("\n");
-        out.println("Enter your nickname: ");
+        out.println("Please enter your nickname: ");
         String nickname = input.nextLine();
+        out.println("Welcome : " + nickname);
         controller.sendMessage(new MessageLogin(nickname));
     }
 
     @Override
     public void askNumPlayer() {
         out.println("\n");
-        out.println("Enter the number of players: ");
+        out.println("Please enter the number of players: ");
         int numPlayers = input.nextInt();
         controller.sendMessage(new MessageNumPlayers(playerBoard.getNickname(), numPlayers));
     }
 
     @Override
-    public void GameStart() {
-
+    public void GameStart(){
+        ViewStart viewstart = new ViewStart();
     }
+
 
     @Override
     public void askChooseLeaderCards() {
         showLeaderActionBox();
-        out.println("\n");
-        out.println("Enter the first Leader card [0-3]: ");
+        out.println("Please enter which two cards you want keep during the game: [0-3]");
         int i1 = input.nextInt();
-
-        out.println("Enter the second Leader card [0-3]: ");
         int i2 = input.nextInt();
-
         controller.sendMessage(new MessageChooseLeaderCards(playerBoard.getNickname(), i1, i2));
-    }
-
-    @Override
-    public void askChooseResourcesFirstTurn(int num) {
-        if (num == 1) {
-            out.println("Enter a resource: ");
-            String resource = input.nextLine();
-            List<String> resourceList = new ArrayList<>();
-            resourceList.add(resource);
-            controller.sendMessage(new MessageChooseResourcesFirstTurn(playerBoard.getNickname(), resourceList));
-        }
-        else {
-            if (num == 2) {
-                List<String> resourceList = new ArrayList<>();
-                out.println("Enter the first resource: ");
-                String resource1 = input.nextLine();
-                resourceList.add(resource1);
-
-
-                out.println("Enter the second resource: ");
-                String resource2 = input.nextLine();
-                resourceList.add(resource2);
-
-                controller.sendMessage(new MessageChooseResourcesFirstTurn(playerBoard.getNickname(), resourceList));
-            }
-        }
-    }
-
-    @Override
-    public void askChooseTurn() {
-        out.println("Actions: \n0: Extract marbles from Market tray\n1: Buy a Development card\n2: Choose resources to activate a base production\n3: Activate production of a Development card\n4: Active production of the Leader card\n5: Activate or discard a Leader card");
-        out.println("Enter the relative number: ");
-        int action = input.nextInt();
-
-        //controller.sendMessage(new MessageChooseTurn(playerBoard.getNickname(), action));
-    }
-
-    @Override
-    public void extractMarble() {
-
-    }
-
-    @Override
-    public void manageMarble() {
-
-    }
-
-    @Override
-    public void exchangeWarehouse() {
-
-    }
-
-    @Override
-    public void buyDevCard() {
-
-    }
-
-    @Override
-    public void payResources() {
-
-    }
-
-    @Override
-    public void activeBaseProduction() {
-
-    }
-
-    @Override
-    public void activeLeaderActionProd() {
-
-    }
-
-    @Override
-    public void activeDevCardProduction() {
-
-    }
-
-    @Override
-    public void updateLeaderCard() {
-
-    }
-
-    @Override
-    public void endTurn() {
-
-    }
-
-    @Override
-    public void endProduction() {
-
-    }
-
-    @Override
-    public void show() {
-
-    }
-
-
-    public void askExtractMarble() {
-        showMarketTray();
-        out.println("\n");
-
-        out.println("Do you want to extract a column or a row? [c/r]: ");
-        String colrows = input.nextLine();
-        char colrow = colrows.charAt(0);
-        int num = 0;
-
-        //se scrive 'r'
-        if (colrow == 'r') {
-            out.println("Which number of row do you want to extract? [0-2]: ");
-            num = input.nextInt();
-        }
-
-        //se scrive 'c'
-        if (colrow == 'r') {
-            out.println("Which number of column do you want to extract? [0-3]: ");
-            num = input.nextInt();
-        }
-
-        controller.sendMessage(new MessageExtractionMarbles(playerBoard.getNickname(), colrow, num));
-    }
-
-
-    public void askAfterTakeMarble() {
-        showMarbleBuffer();
-        out.println("\n");
-        showWarehouse();
-        out.println("\n");
-
-        out.println("Actions:\n0: Exchange two rows of the Warehouse depots\n1: Add or discard an extracted marble (if you discard a marble, you'll give a faith point to each other player)\n2: Choose with which resource you'll convert an extracted white marble (if yuo have the special effect of the relative Leader card)");
-        out.println("Enter the relative action's number: ");
-        int choice = input.nextInt();
-
-        //controller.sendMessage(new MessageChooseAfterTakeMarble(playerBoard.getNickname(), choice));
-    }
-
-
-    public void askExchange() {
-        showWarehouse();
-        out.println("\n");
-
-        out.println("Enter the first row to exchange: ");
-        int row1 = input.nextInt();
-
-        out.println("Enter the second row to exchange: ");
-        int row2 = input.nextInt();
-
-        controller.sendMessage(new MessageExchangeWarehouse(playerBoard.getNickname(), row1, row2));
-    }
-
-    public void askAddDiscardMarble() {
-        showMarbleBuffer();
-        out.println("\n");
-        showWarehouse();
-        out.println("\n");
-
-        out.println("0: Discard this marble\n1: Add this marble to the Warehouse depots");
-        out.println("Enter your choice: ");
-        int choiceb = input.nextInt();
-
-        if (choiceb == 0) {
-            //controller.sendMessage(new MessageAddDiscardMarble(playerBoard.getNickname(), false, -1));
-        }
-        else if (choiceb == 1){
-            out.println("Enter Warehouse depots' index in which you want to insert the resource [0-2]: ");
-            int index = input.nextInt();
-           //  controller.sendMessage(new MessageAddDiscardMarble(playerBoard.getNickname(), true, index));
-        }
-    }
-
-    public void askSelectTrasformationWhiteMarble() {
-        showWhiteMarbleEffectList();
-        out.println("\n");
-
-        out.println("Enter the resource with which do you want to convert the White marble [0-1]: ");
-        int choice = input.nextInt();
-
-        //controller.sendMessage(new MessageSelectTransformationWhiteMarble(playerBoard.getNickname(), choice));
-    }
-
-
-    public void askSelectDevCard() {
-        boolean wantToWatch = true;
-
-        showDevCardDeck();
-        out.println("\n");
-
-        while (wantToWatch) {
-            out.println("Do you want to see a Development card? [0-1]: ");
-            int choice = input.nextInt();
-            if (choice == 0)
-                wantToWatch = false;
-            else if (choice == 1){
-                out.println("Enter the Development card's ID: ");
-                String card = input.nextLine();
-                showDevCard(card);
-            }
-        }
-
-        out.println("Enter the Development card's ID to buy: ");
-        String devCard = input.nextLine();
-
-        //controller.sendMessage(new MessageSelectDevCard(playerBoard.getNickname(), devCard));
-    }
-
-    public void askChooseResourcesPurchaseDevCard() {
-        showWarehouse();
-        out.println("\n");
-
-        Map<String,Integer> WarehouseRes = new HashMap<>();
-        out.println("Enter the type of resources from the Warehouse depots [Coins, Servants, Shields, Stones]: ");
-        String w = input.nextLine();
-
-        out.println("Enter the number of resources: ");
-        int numW = input.nextInt();
-
-        WarehouseRes.put(w, numW);
-
-        showStrongbox();
-        out.println("\n");
-
-        Map<String,Integer> StrongboxRes = new HashMap<>();
-        out.println("Enter the type of resources from the Strongbox [Coins, Servants, Shields, Stones]: ");
-        String s = input.nextLine();
-
-        out.println("Enter the number of resources: ");
-        int numS = input.nextInt();
-
-        StrongboxRes.put(s, numS);
-
-        Map<String,Integer> ExtrachestMap = new HashMap<>();
-        if (!playerBoard.getExtrachest().isEmpty()) {
-            showExtraChest();
-            out.println("\n");
-
-            out.println("Enter the type of resources from the Extra Chest [Coins, Servants, Shields, Stones]: ");
-            String e = input.nextLine();
-
-            out.println("Enter the number of resources: ");
-            int numE = input.nextInt();
-
-            ExtrachestMap.put(e, numE);
-        }
-
-        //controller.sendMessage(new MessageChooseResourcesPurchaseDevCard(playerBoard.getNickname(), WarehouseRes, StrongboxRes, ExtrachestMap));
-    }
-
-    public void askInsertCard() {
-        boolean wantToWatch = true;
-
-        showSlotDevCard();
-        out.println("\n");
-
-
-        while (wantToWatch) {
-            out.println("Do you want to see a Development card? [0-1]: ");
-            int choice = input.nextInt();
-            if (choice == 0)
-                wantToWatch = false;
-            else if (choice == 1){
-                out.println("Enter the Development card's ID: ");
-                String card = input.nextLine();
-                showDevCard(card);
-            }
-        }
-
-        out.println("Enter the slot in which do you want to insert the card [0-2]: ");
-        int choice = input.nextInt();
-
-        //controller.sendMessage(new MessageInsertCard(playerBoard.getNickname(), choice));
-    }
-
-    public void askProductionType() {
-        boolean wantToWatch = true;
-
-        /*
-        while (wantToWatch) {
-            out.println("Do you want to see the Warehouse depots? [0-1]: ");
-            if (!input)
-                wantToWatch = false;
-            else {
-                showWarehouse();
-                wantToWatch = false;
-            }
-        }
-
-        wantToWatch = true;
-
-        while (wantToWatch) {
-            out.println("Do you want to see the Strongbox? [0-1]: ");
-            if (!input)
-                wantToWatch = false;
-            else {
-                showStrongbox();
-                wantToWatch = false;
-            }
-        }
-
-        if (!playerBoard.getExtrachest().isEmpty()) {
-            wantToWatch = true;
-
-            while (wantToWatch) {
-                out.println("Do you want to see the Extra chest? [0-1]: ");
-                if (!input)
-                    wantToWatch = false;
-                else {
-                    showExtraChest();
-                    wantToWatch = false;
-                }
-            }
-        }
-
-        showSlotDevCard();
-
-        wantToWatch = true;
-
-        while (wantToWatch) {
-            out.println("Do you want to see a Development card? [0-1]: ");
-            if (!input)
-                wantToWatch = false;
-            else {
-                out.println("Enter the Development card's ID: ");
-                showDevCard(input);
-            }
-        }
-
-         */
-
-        out.println("Productions:\n0: Activate base production\n1: Activate the production power of a Development card\n2: Activate the production power of the relative Leader card\n3: End production");
-    }
-
-    public void askActiveBaseProduction() {
-        showWarehouse();
-        out.println("Enter the type of resources from the Warehouse depots [Coins, Servants, Shields, Stones]: ");
-
-        out.println("Enter the number of resources: ");
-
-        showStrongbox();
-        out.println("Enter the type of resources from the Strongbox [Coins, Servants, Shields, Stones]: ");
-
-        out.println("Enter the number of resources: ");
-
-        if (!playerBoard.getExtrachest().isEmpty()) {
-            showExtraChest();
-            out.println("Enter the type of resources from the Extra Chest [Coins, Servants, Shields, Stones]: ");
-
-            out.println("Enter the number of resources: ");
-        }
-    }
-
-    public void askChosenResourceBaseProduction() {
-        out.println("Enter the type of the chosen resource [Coins, Servants, Shields, Stones]: ");
-    }
-
-
-    public void askActiveProductionDevCard() {
-        boolean wantToWatch = true;
-
-        showSlotDevCard();
-
-        /*
-        while (wantToWatch) {
-            out.println("Do you want to see a Development card? [0-1]: ");
-            if (!input)
-                wantToWatch = false;
-            else {
-                out.println("Enter the Development card's ID: ");
-                showDevCard(input);
-            }
-        }
-
-         */
-
-        out.println("Enter the number of the Slot Development cards' column [0-2]: ");
-    }
-
-
-    public void askChooseResourcesDevCardProduction() {
-        showWarehouse();
-        out.println("Enter the type of resources from the Warehouse depots [Coins, Servants, Shields, Stones]: ");
-
-        out.println("Enter the number of resources: ");
-
-        showStrongbox();
-        out.println("Enter the type of resources from the Strongbox [Coins, Servants, Shields, Stones]: ");
-
-        out.println("Enter the number of resources: ");
-
-        if (!playerBoard.getExtrachest().isEmpty()) {
-            showExtraChest();
-            out.println("Enter the type of resources from the Extra Chest [Coins, Servants, Shields, Stones]: ");
-
-            out.println("Enter the number of resources: ");
-        }
-    }
-
-
-    public void askActiveLeaderCardProduction() {
-        boolean wantToWatch = true;
-
-        showLeaderActionBox();
-
-        /*
-        while (wantToWatch) {
-            out.println("Do you want to see a Leader card? [0-1]: ");
-            if (!input)
-                wantToWatch = false;
-            else {
-                out.println("Enter the Leader card's ID: ");
-                showLeaderAction(input);
-            }
-        }
-
-         */
-
-        out.println("Enter the index of the Leader card [0-1]: ");
-
-        showWarehouse();
-        showStrongbox();
-        if (!playerBoard.getExtrachest().isEmpty())
-            showExtraChest();
-
-        out.println("Depots:\n0: Warehouse depots\n1: Strongbox\n2: ExtraChest(if you have)");
-        out.println("Enter the relative number from where you want to take the resource: ");
-
-        out.println("Enter the type of the given resource [Coins, Servants, Shields, Stones]: ");
-    }
-
-
-    public void sendEndProduction() {
-
-    }
-
-
-    public void askEndTurnActiveLeaderCard() {
-        boolean wantToWatch = true;
-
-        showLeaderActionBox();
-
-        /*
-        while (wantToWatch) {
-            out.println("Do you want to see a Leader card? [0-1]: ");
-            if (!input)
-                wantToWatch = false;
-            else {
-                out.println("Enter the Leader card's ID: ");
-                showLeaderAction(input);
-            }
-        }
-
-         */
-
-        out.println("Do you want to activate or discard a Leader card or do you want to end your turn? [0-1]: ");
-    }
-
-
-    public void askUpdateStateLeaderAction() {
-        boolean wantToWatch = true;
-
-        showLeaderActionBox();
-
-        /*
-        while (wantToWatch) {
-            out.println("Do you want to see a Leader card? [0-1]: ");
-            if (!input)
-                wantToWatch = false;
-            else {
-                out.println("Enter the Leader card's ID: ");
-                showLeaderAction(input);
-            }
-        }
-
-         */
-
-        out.println("Which card do you want to activate or discard [0-1]: ");
-
-        out.println("Do you want to activate or discard this card? [0-1]: ");
     }
 
     //show
@@ -650,8 +172,21 @@ public class Cli implements ViewInterface {
 
     }
 
-    @Override
-    public void ioError() {
-
+    public void help(){
+        System.out.println("Here the commands you can insert during the game: " +
+                "\n extractionmarble <row or column (r/c)> < index of r/c>" +
+                "\n managemarble <W/E/D (warehouse,extrachest,discard)> (optional)<index of W> <resources wanted if changeWitheMarble is activated>" +
+                "\n exchangewarehouse <row1> <row2>" +
+                "\n buydevcard <id card> <position where store the card in slotdevcard>" +
+                "\n payresources <W/S/E (warehouse/strongbox/extrachest)> <resource> <how many>" +
+                "\n activebaseproduction <resource wanted> <W/S/E> <resource paied> <W/S/E> <resource paied>" +
+                "\n activeleaderaction <id card> <W/S/E> <resource>" +
+                "\n activedevcardproduction <id card>" +
+                "\n updateleadercard <id card> <0 discard 1 active>" +
+                "\n endturn" +
+                "\n endproduction" +
+                "\n show <structure to show>" +
+                "\n help ");
     }
+
 }
