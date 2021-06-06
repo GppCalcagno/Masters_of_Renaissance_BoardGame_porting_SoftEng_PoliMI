f8a42bad8b2b2ff73a29bfecc6246e2817760e9c
Giuseppe-Calcagno <79711772+Giuseppe-Calcagno@users.noreply.github.com>
2021-06-06 11:59:38 +0200

    add windows app

---
+++ src/main/java/it/polimi/ingsw/ClientGUIWindows.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.Client.ClientController;
+
+public class ClientGUIWindows {
+
+    public static void main(String[] args) {
+        new ClientController(false,true);
+    }
+}
